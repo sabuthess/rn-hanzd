e6e642d9dc539c1918b5a3a803248de1b7051212
sabuthess <dilan.developer22@gmail.com>
2026-03-09 21:08:31 -0500

    initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,11 @@
+import { useColorScheme } from '@/hooks/use-color-scheme';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -15,10 +13,25 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+
+        <Stack.Screen
+          name="index"
+          options={{
+            headerStyle: { backgroundColor: "#fafafa" },
+            headerShadowVisible: false,
+            headerTitle: "All songs",
+            headerRight: () => (
+              <>
+                <Icon name="search-outline" size={24} color="#333"  />
+                <Icon name="ellipsis-vertical" size={24} color="#333"  />
+              </>
+            ),
+          }}
+        />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
+
