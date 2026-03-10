ad8b683814c576f92dbef266e9cfd2fbfa1693d5
sabuthess <dilan.developer22@gmail.com>
2026-03-09 21:08:56 -0500

    Add songs screens

---
+++ screens/SongsScreen.tsx
@@ -0,0 +1,143 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
+export default function SongsScreen() {
+    type ItemsProps = { title: string, artist: string }
+
+    const DATA = [
+        { id: "1", title: "Al2 - Alas", artist: "Unknown artist- Download" },
+        { id: "2", title: "Al2 - Rojo", artist: "Unknown artist- Download" },
+        { id: "3", title: "Al2 - A veces", artist: "Unknown artist- Download" },
+        { id: "4", title: "Al2 - 3 de la mañana", artist: "Unknown artist- Download" },
+        { id: "5af", title: "Al2 - Milagro", artist: "Unknown artist- Download" },
+        { id: "fasd5", title: "Al2 - Milagro", artist: "Unknown artist- Download" },
+        { id: "5afsd", title: "Al2 - Milagro", artist: "Unknown artist- Download" },
+        { id: "5fsa", title: "Al2 - Milagro", artist: "Unknown artist- Download" },
+        { id: "5asdf", title: "Al2 - Milagro", artist: "Unknown artist- Download" },
+        { id: "5sdfasdf", title: "Al2 - Milagro", artist: "Unknown artist- Download" },
+        { id: "5afdsfa", title: "Al2 - Milagro", artist: "Unknown artist- Download" },
+        { id: "safd5", title: "Al2 - Milagro", artist: "Unknown artist- Download" },
+        { id: "sdf5", title: "Al2 - Milagro", artist: "Unknown artist- Download" },
+        { id: "5sdfa", title: "Al2 - Milagro", artist: "Unknown artist- Download" },
+        { id: "5asdfd", title: "Al2 - Milagro", artist: "Unknown artist- Download" },
+        { id: "afs5", title: "Al2 - Milagro", artist: "Unknown artist- Download" },
+    ]
+
+    const SongItemCard = ({ title, artist }: ItemsProps) => (
+        <View style={styles.item}>
+            <View>
+                <Text style={styles.title}>{title}</Text>
+                <Text style={styles.artist}>{artist}</Text>
+            </View>
+            <View style={styles.container_icons}>
+
+                <Icon name="add-circle-outline" size={24} color="#333"  />
+                <Icon name="ellipsis-vertical" size={24} color="#333"  />
+            </View>
+        </View>
+    )
+
+    return (<>
+        <View>
+            <View style={styles.container_info_songs}>
+                <View style={styles.container_info_btns}>
+                    <View style={styles.container_btn_play}>
+                        <Icon name="play-outline" size={24} color="#333"  />
+                    </View>
+                    <Text>Play all (242) </Text>
+                </View>
+                <View style={styles.container_info_btns}>
+                    <Icon name="list-outline" size={24} color="#333"  />
+                    <Icon name="swap-vertical" size={24} color="#333"  />
+
+                </View>
+
+            </View>
+
+        </View>
+
+        <FlatList
+            style={styles.container_list}
+            showsVerticalScrollIndicator={false}
+            scrollEnabled={true}
+            data={DATA}
+            renderItem={({ item }) => <SongItemCard title={item.title} artist={item.artist} />}
+            keyExtractor={item => item.id}
+        />
+    </>
+    )
+}
+
+const styles = StyleSheet.create({
+    container_list: {
+        display: "flex",
+        flexDirection: "column",
+        gap: 30,
+        marginHorizontal: 20,
+    },
+    container_scroll: {
+        marginVertical: 10,
+    },
+
+    container: {
+        flex: 1,
+        marginTop: 50,
+        backgroundColor: "#ffffffc9"
+    },
+    item: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        // backgroundColor: "#beffc6",
+        borderRadius: 4,
+        paddingVertical: 10,
+        marginVertical: 5,
+        borderBottomWidth: 1,
+        borderBottomColor: "#d4d4d4"
+    },
+    title: {
+        fontSize: 17,
+        fontWeight: "medium"
+    },
+    artist: {
+        color: "#7a7a7a"
+    },
+
+    container_icons: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 10,
+    },
+
+
+    container_info_songs: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginVertical: 20,
+        paddingHorizontal: 20,
+    },
+
+    container_btn_play: {
+        height: 30,
+        width: 30,
+        backgroundColor: "#16a085",
+        borderRadius: 50,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginRight: 10,
+        color: "#fff",
+    },
+    container_info_btns: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 10,
+    },
+    item_info: {
+        marginVertical: 10
+    }
+});
