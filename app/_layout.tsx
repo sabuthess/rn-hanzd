fa7f8cc14c21f314cecc53cf8d06af0497ca032f
sabuthess <dilan.developer22@gmail.com>
2026-03-11 08:25:04 -0500

    Add song modal screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,8 @@
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { TouchableOpacity } from 'react-native';
 import 'react-native-reanimated';
 import Icon from 'react-native-vector-icons/Ionicons';
 
@@ -9,6 +10,7 @@ import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const router = useRouter();
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
@@ -22,14 +24,27 @@ export default function RootLayout() {
             headerTitle: "All songs",
             headerRight: () => (
               <>
-                <Icon name="search-outline" size={24} color="#333"  />
-                <Icon name="ellipsis-vertical" size={24} color="#333"  />
+                <Icon name="search-outline" size={24} color="#333" />
+                <Icon name="ellipsis-vertical" size={24} color="#333" />
               </>
             ),
           }}
         />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen name='songs/[id]' options={{
+          presentation: "transparentModal",
+          animation: "slide_from_bottom",
+          headerShadowVisible: false,
+          headerTitleAlign: "center",
+          headerTitle: "Song",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()} style={{ paddingHorizontal: 16 }}>
+              <Icon name="chevron-down" size={24} color="#333" />
+            </TouchableOpacity>
+          ),
+
+        }} />
       </Stack>
+
       <StatusBar style="auto" />
     </ThemeProvider>
   );
