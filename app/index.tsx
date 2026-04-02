2731a94d8aa3730f2474288be073a39d58cee965
sabuthess <dilan.developer22@gmail.com>
2026-04-02 12:35:21 -0500

    Remove safe area and add song footer

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { SongFooter } from "@/components/ui/song-footer";
 import React, { useEffect, useRef, useState } from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import PagerView from "react-native-pager-view";
@@ -6,6 +7,7 @@ import SongsScreen from "./songs";
 export default function HomeScreen() {
   const [activePage, setActivePage] = useState(0);
   const pagerRef = useRef<PagerView>(null);
+
   const showScreen = (id: number) => {
     pagerRef.current?.setPage(id);
     setActivePage(id);
@@ -14,113 +16,108 @@ export default function HomeScreen() {
   useEffect(() => {}, [pagerRef]);
 
   return (
-    <View style={styles.root}>
-      <View style={styles.container_scroll}>
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={styles.scrollContent}
-        >
-          <Text
-            style={activePage === 0 ? styles.button_active : styles.button}
-            onPress={() => showScreen(0)}
+    <>
+      {/* <SafeAreaView style={{ flex: 1, backgroundColor: "#eee" }}> */}
+      <View style={styles.root}>
+        <View style={styles.container_scroll}>
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.scrollContent}
           >
-            Songs
-          </Text>
-          <Text
-            style={activePage === 1 ? styles.button_active : styles.button}
-            onPress={() => showScreen(1)}
-          >
-            Favorites
-          </Text>
-          <Text
-            style={activePage === 2 ? styles.button_active : styles.button}
-            onPress={() => showScreen(2)}
-          >
-            Playlist
-          </Text>
-          <Text
-            style={activePage === 3 ? styles.button_active : styles.button}
-            onPress={() => showScreen(3)}
-          >
-            Artist
-          </Text>
-          <Text
-            style={activePage === 4 ? styles.button_active : styles.button}
-            onPress={() => showScreen(4)}
-          >
-            Albums
-          </Text>
-          <Text
-            style={activePage === 5 ? styles.button_active : styles.button}
-            onPress={() => showScreen(5)}
-          >
-            Folders
-          </Text>
-        </ScrollView>
-      </View>
+            <Text
+              style={activePage === 0 ? styles.button_active : styles.button}
+              onPress={() => showScreen(0)}
+            >
+              Songs
+            </Text>
+            <Text
+              style={activePage === 1 ? styles.button_active : styles.button}
+              onPress={() => showScreen(1)}
+            >
+              Favorites
+            </Text>
+            <Text
+              style={activePage === 2 ? styles.button_active : styles.button}
+              onPress={() => showScreen(2)}
+            >
+              Playlist
+            </Text>
+            <Text
+              style={activePage === 3 ? styles.button_active : styles.button}
+              onPress={() => showScreen(3)}
+            >
+              Artist
+            </Text>
+            <Text
+              style={activePage === 4 ? styles.button_active : styles.button}
+              onPress={() => showScreen(4)}
+            >
+              Albums
+            </Text>
+            <Text
+              style={activePage === 5 ? styles.button_active : styles.button}
+              onPress={() => showScreen(5)}
+            >
+              Folders
+            </Text>
+          </ScrollView>
+        </View>
 
-      <View style={styles.container_screens}>
-        <PagerView
-          onPageSelected={(e) => setActivePage(e.nativeEvent.position)}
-          ref={pagerRef}
-          style={styles.container}
-          initialPage={0}
-        >
-          <View key="1">
-            <SongsScreen />
-          </View>
-          <View key="2">
-            {/* <FavoritesScreen /> */}
-            <Text>2</Text>
-          </View>
-          <View key="3">
-            <Text>3</Text>
-          </View>
-          <View key="4">
-            <Text>4</Text>
-          </View>
-          <View key="5">
-            <Text>5</Text>
-          </View>
-        </PagerView>
+        <View style={styles.container_screens}>
+          <PagerView
+            onPageSelected={(e) => setActivePage(e.nativeEvent.position)}
+            ref={pagerRef}
+            style={{ flex: 1 }}
+            initialPage={0}
+          >
+            <SongsScreen key="1" />
+            <View key="2">
+              {/* <FavoritesScreen /> */}
+              <Text>2</Text>
+            </View>
+            <View key="3">
+              <Text>3</Text>
+            </View>
+            <View key="4">
+              <Text>4</Text>
+            </View>
+            <View key="5">
+              <Text>5</Text>
+            </View>
+          </PagerView>
+        </View>
       </View>
-    </View>
+      <SongFooter />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    backgroundColor: "#fafafa",
+    backgroundColor: "#eee",
   },
+
+  scrollContent: {},
+
   container_scroll: {
+    display: "flex",
+    gap: 30,
     maxHeight: 70,
   },
-  scrollContent: {},
+
   container_screens: {
-    flex: 1,
-  },
-  container: {
     flex: 1,
   },
 
   button: {
-    backgroundColor: "none",
-    marginHorizontal: 10,
-    fontSize: 16,
     color: "#000",
+    marginHorizontal: 20,
   },
 
   button_active: {
-    backgroundColor: "none",
-    marginHorizontal: 10,
-    fontSize: 16,
-
+    marginHorizontal: 20,
     color: "#16a085",
-  },
-
-  active_screen_text: {
-    color: "blueviolet",
   },
 });
