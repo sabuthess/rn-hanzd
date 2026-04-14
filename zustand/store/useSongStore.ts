8f390be324b3ca70f86b93dd7d23ba282351afee
sabuthess <dilan.developer22@gmail.com>
2026-04-14 14:11:34 -0500

    Change audio player for react native track player

--- zustand/store/useSongStore.ts
+++ zustand/store/useSongStore.ts
@@ -1,70 +1,12 @@
 import { ISongStore } from "@/interfaces/song-store.interface";
 import { ISong } from "@/interfaces/song.interface";
 import { storage } from "@/utils/mmkv";
-import { Asset } from "expo-asset";
-import type { AudioPlayer } from "expo-audio";
-import { createAudioPlayer } from "expo-audio";
+import TrackPlayer from "react-native-track-player";
+
 import { create } from "zustand";
 
-const loadAsset = async () => {
-  const asset = Asset.fromModule(
-    require("../../assets/images/child-background-img.jpg"),
-  );
-  await asset.downloadAsync();
-  return asset.uri;
-};
-/* =========================
-   💾 STORAGE
-========================= */
 const storedSongs = storage.getString("@local_songs");
 const parsedSongs: ISong[] = storedSongs ? JSON.parse(storedSongs) : [];
-
-/* =========================
-   🔐 HELPERS
-========================= */
-const safeRelease = async (
-  player?: AudioPlayer | null,
-  subscription?: any,
-  interval?: any,
-) => {
-  try {
-    subscription?.remove?.();
-
-    if (interval) {
-      clearInterval(interval);
-    }
-
-    if (!player) return;
-
-    await player.pause().catch(() => {});
-    player.release();
-  } catch {}
-};
-const setupLockScreen = async (player: AudioPlayer, song: ISong) => {
-  try {
-    const uri = await loadAsset();
-
-    player.setActiveForLockScreen(true, {
-      title: song.title ?? "Unknown",
-      artist: song.artist ?? "Unknown",
-      albumTitle: song.album ?? "",
-      artworkUrl: uri,
-    });
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const createNewPlayer = async (song: ISong) => {
-  const player = await createAudioPlayer({
-    source: { uri: song.uri },
-    updateInterval: 250,
-  });
-
-  setupLockScreen(player, song);
-
-  return { player };
-};
 
 /* =========================
    🧠 STORE
@@ -86,18 +28,12 @@ export const useSongStore = create<ISongStore>((set, get) => ({
 
   currentPlayId: null as number | null,
 
-  /* =========================
-     🎶 SET SONGS
-  ========================= */
   setSongs: (songs) => set({ songs }),
 
-  /* =========================
-     🎶 SET QUEUE
-  ========================= */
   setQueue: async (songs, startIndex = 0) => {
-    const { player, subscription, trackingInterval } = get();
+    // const { player, subscription, trackingInterval } = get();
 
-    await safeRelease(player, subscription, trackingInterval);
+    // await safeRelease(player, subscription, trackingInterval);
 
     set({
       queue: songs,
@@ -109,60 +45,35 @@ export const useSongStore = create<ISongStore>((set, get) => ({
     await get().playCurrent();
   },
 
-  /* =========================
-     ▶️ PLAY CURRENT
-  ========================= */
   playCurrent: async () => {
-    const {
-      queue,
-      currentIndex,
-      player: oldPlayer,
-      subscription,
-      trackingInterval,
-    } = get();
-    if (!queue.length) return;
+    const { queue, currentIndex } = get();
 
-    // Limpia el player anterior
-    await safeRelease(oldPlayer, subscription, trackingInterval);
+    if (!queue.length) return;
+    if (currentIndex < 0 || currentIndex >= queue.length) return;
 
     const song = queue[currentIndex];
+    if (!song?.uri) return;
 
-    let newPlayer;
-    try {
-      const result = await createNewPlayer(song);
-      newPlayer = result.player;
-      if (!newPlayer) throw new Error("Player no se pudo crear");
-    } catch (e) {
-      console.error("Error creando AudioPlayer:", e);
-      return;
-    }
+    // await initTrackPlayer();
 
-    // Suscríbete al evento de progreso
-    const newSub = newPlayer.addListener("statusChange", (status) => {
-      console.log("STATUS:", status);
+    await TrackPlayer.stop();
+    await TrackPlayer.reset();
 
-      set({
-        currentTime: status.currentTime ?? 0,
-        duration: status.duration ?? 0,
-      });
-    });
+    await TrackPlayer.add(
+      queue.map((song) => ({
+        id: song.id.toString(),
+        url: song.uri,
+        title: song.title,
+        artist: song.artist,
+        artwork: require("../../assets/images/child-background-img.jpg"),
+      })),
+    );
 
-    // Actualiza el store con el player y subscription
     set({
-      player: newPlayer,
-      subscription: newSub,
       isPlaying: false,
       currentTime: 0,
       duration: 0,
     });
-
-    try {
-      await newPlayer.play();
-      set({ isPlaying: true });
-    } catch (e) {
-      console.error("Error reproduciendo canción:", e);
-      set({ isPlaying: false });
-    }
   },
   /* =========================
      ▶️ PLAY BY ID
@@ -179,53 +90,28 @@ export const useSongStore = create<ISongStore>((set, get) => ({
      ▶️ / ⏸ TOGGLE
   ========================= */
   togglePlay: async () => {
-    const player = get().player;
-    if (!player) return;
-
     const { isPlaying } = get();
 
     if (isPlaying) {
-      await player.pause();
+      await TrackPlayer.pause();
       set({ isPlaying: false });
     } else {
-      await player.play();
+      await TrackPlayer.play();
       set({ isPlaying: true });
     }
   },
-
   /* =========================
      ⏭ NEXT
   ========================= */
   handleNext: async () => {
-    const { queue } = get();
-    if (!queue.length) return;
-
-    // Elegir un índice aleatorio distinto al actual
-    let nextIndex;
-    do {
-      nextIndex = Math.floor(Math.random() * queue.length);
-    } while (nextIndex === get().currentIndex && queue.length > 1);
-
-    set({ currentIndex: nextIndex });
-    await get().playCurrent();
+    await TrackPlayer.skipToNext();
   },
-
   /* =========================
      ⏮ PREV
   ========================= */
   handlePrev: async () => {
-    const { queue } = get();
-    if (!queue.length) return;
-
-    let prevIndex;
-    do {
-      prevIndex = Math.floor(Math.random() * queue.length);
-    } while (prevIndex === get().currentIndex && queue.length > 1);
-
-    set({ currentIndex: prevIndex });
-    await get().playCurrent();
+    await TrackPlayer.skipToPrevious();
   },
-
   /* =========================
      ➕ ADD TO QUEUE
   ========================= */
